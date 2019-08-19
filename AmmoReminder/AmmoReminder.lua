65ded36f9510609d39b741ebc05018842f7e773a
Bruno Castro-Karney <brunope@cs.uw.edu>
2019-08-18 19:45:42 -0700

    Add arrow vendors for major horde cities

--- AmmoReminder/AmmoReminder.lua
+++ AmmoReminder/AmmoReminder.lua
@@ -1,49 +1,71 @@
 local hordeArrowSubzoneToNpc = {
-	["Darkspear Training Grounds"]="Test",
-	["Bloodhoof Village"]="Moorat Longstride",
-	["Bloodvenom Post"]="Bale",
-	["Booty Bay"]="Haren Kanmae",
-	["Brackenwall Village"]="Zanara",
-	["Brill"]="Mrs. Winters",
-	["Camp Mojache"]="Cawind Trueaim",
-	["Camp Narache"]="Kawnie Softbreeze",
-	["Cenarion Hold"]="Calandrath",
-	["Deathknell"]="Joshua Kien",
-	["Freewind Post"]="Starn",
-	["Grom'gol Base Camp"]="Uthok",
-	["Hammerfall"]="Graud",
-	["Light's Hope Chapel"]="Caretaker Alen",
-	["Marshal's Refuge"]="Nergal",
-	["Nighthaven"]="Daeolyn Summerleaf",
-	["Ratchet"]="Jazzik",
-	["Raventusk Village"]="Renn'az",
-	["Razor Hill"]="Ghrawt",
-	["Sen'jin Village"]="Trayexir",
-	["Shadowprey Village"]="Tukk",
-	["Splintertree Post"]="Burkrum",
-	["Steamwheedle Port"]="Jabbey",
-	["Stonetalon Mountains"]="Grawnal",
-	["Tarren Mill"]="Kayren Soothallow",
-	["The Crossroads"]="Uthrok",
-	["The Sepulcher"]="Edwin Harly",
-	["Thorium Point"]="Master Smith Burninate",
-	["Valley of Trials"]="Duokna"
+    ["Darkspear Training Grounds"]="Test",
+    ["Bloodhoof Village"]="Moorat Longstride",
+    ["Bloodvenom Post"]="Bale",
+    ["Booty Bay"]="Haren Kanmae",
+    ["Brackenwall Village"]="Zanara",
+    ["Brill"]="Mrs. Winters",
+    ["Camp Mojache"]="Cawind Trueaim",
+    ["Camp Narache"]="Kawnie Softbreeze",
+    ["Cenarion Hold"]="Calandrath",
+    ["Deathknell"]="Joshua Kien",
+    ["Freewind Post"]="Starn",
+    ["Grom'gol Base Camp"]="Uthok",
+    ["Hammerfall"]="Graud",
+    ["Light's Hope Chapel"]="Caretaker Alen",
+    ["Marshal's Refuge"]="Nergal",
+    ["Nighthaven"]="Daeolyn Summerleaf",
+    ["Ratchet"]="Jazzik",
+    ["Raventusk Village"]="Renn'az",
+    ["Razor Hill"]="Ghrawt",
+    ["Sen'jin Village"]="Trayexir",
+    ["Shadowprey Village"]="Tukk",
+    ["Splintertree Post"]="Burkrum",
+    ["Steamwheedle Port"]="Jabbey",
+    ["Stonetalon Mountains"]="Grawnal",
+    ["Tarren Mill"]="Kayren Soothallow",
+    ["The Crossroads"]="Uthrok",
+    ["The Sepulcher"]="Edwin Harly",
+    ["Thorium Point"]="Master Smith Burninate",
+    ["Valley of Trials"]="Duokna"
 }
 
+local hordeArrowCityToNpc = {
+    ["Orgrimmar"]="Trak'gen",
+    ["Undercity"]="Eleanor Rusk",
+    ["Thunder Bluff"]="Kuruk"
+}
+
+local function Remind(npc)
+   message("Remember to buy arrows! Look for "..npc)
+end
+
+local function FindHordeArrowVendorNpc(zone, subzone)
+    local npc = hordeArrowSubzoneToNpc[subzone]
+    if npc ~= nil then
+        return npc
+    end
+
+    npc = hordeArrowCityToNpc[zone]
+    if npc ~= nil then
+        return npc
+    end
+end
+
 local function HandleZoneChange(self, event, ...)
-	local subzone = GetSubZoneText()
-	local zone = GetZoneText()
-	print("Checking for ammo vendors in "..subzone..", "..zone)
-	local npc = hordeArrowSubzoneToNpc[subzone]
-	if npc ~= nil then
-		message("Remember to buy arrows! Look for "..npc)
-	end
+    local subzone = GetSubZoneText()
+    local zone = GetZoneText()
+    print("Checking for ammo vendors in "..subzone..", "..zone)
+    local npc = FindHordeArrowVendorNpc(zone, subzone)
+    if npc ~= nil then
+           Remind(npc)
+    end
 end
 
 local ZoneChange_EventFrame = CreateFrame("Frame")
 ZoneChange_EventFrame:RegisterEvent("ZONE_CHANGED")
 ZoneChange_EventFrame:SetScript("OnEvent", HandleZoneChange)
-		
+
 local ZoneChangeIndoors_EventFrame = CreateFrame("Frame")
 ZoneChangeIndoors_EventFrame:RegisterEvent("ZONE_CHANGED_INDOORS")
 ZoneChangeIndoors_EventFrame:SetScript("OnEvent", HandleZoneChange)
