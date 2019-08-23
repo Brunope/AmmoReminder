7d863ece2678558507cf5483f7d394f51a9b2440
Bruno Castro-Karney <brunope@cs.uw.edu>
2019-08-22 19:36:16 -0700

    remove some trailing semicolons that maybe were causing errors?

--- AmmoReminder/Ammo.lua
+++ AmmoReminder/Ammo.lua
@@ -7,7 +7,7 @@ function GetEquippedAmmoAmount()
 	local slotId, _ = GetInventorySlotInfo("AmmoSlot");
 	local ammoCount = GetInventoryItemCount("player", slotId);
 	if ammoCount == 1 and not GetInventoryItemTexture("player", slotId) then
-		ammoCount = 0;
-	end;
-	return ammoCount;
-end;
+		ammoCount = 0
+	end
+	return ammoCount
+end
